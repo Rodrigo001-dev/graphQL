f3c646e1112423a428bff5307f72734a08c6130d
Rodrigo <rodrigorael53@gmail.com>
2022-06-10 00:23:37 -0300

    finishin the user model

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,3 +1,11 @@
-class User {
-  
+import { Field, ID, ObjectType } from "type-graphql";
+
+@ObjectType()
+export class User {
+  // para cada campo eu vou colocar um decorator Field()
+  @Field(_type => ID)
+  id: string;
+
+  @Field()
+  name: string;
 };
