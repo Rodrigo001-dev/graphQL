e529c3fea17bc722a3542be47c238b4574570bbc
Rodrigo <rodrigorael53@gmail.com>
2022-06-10 16:31:54 -0300

    creating a user

--- backend/src/resolvers/UserResolver.ts
+++ backend/src/resolvers/UserResolver.ts
@@ -1,12 +1,30 @@
-import { Query, Resolver } from 'type-graphql';
+import { Mutation, Query, Resolver } from 'type-graphql';
+import { User } from '../models/User';
+
+// no GraphQL temos 2 conceitos
+// query: buscar dados
+// mutation: criar, alterar ou deletar
 
 @Resolver()
 export class UserResolver {
   
-  private data = [];
+  private data: User[] = [];
 
-  @Query(() => String)
-  async hello() {
-    return 'Hello World'
+  // no typescript quando queremos informar que é uma lista de informações os []
+  // são colocados no final(User[]), no GraphQL quando queremos retornar uma
+  // lista de informações os [] são colocados envolta da classe([User])
+  @Query(() => [User])
+  async users() {
+    // vai retornar todos os usuários
+    return this.data;
+  };
+
+  @Mutation(() => User)
+  async createUser() {
+    const user = { id: '1', name: 'Rodrigo' };
+
+    this.data.push(user);
+
+    return user;
   };
 };
