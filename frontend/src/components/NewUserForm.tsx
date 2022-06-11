6cf041b0e067c91329d881e8d700fa7098a8e64e
Rodrigo <rodrigorael53@gmail.com>
2022-06-10 23:50:44 -0300

    creating the component to create a new user

---
+++ frontend/src/components/NewUserForm.tsx
@@ -0,0 +1,39 @@
+import { gql, useMutation } from "@apollo/client";
+import { FormEvent, useState } from "react";
+
+const CREATE_USER = gql`
+  mutation ($name: string) {
+    createUser(name: $name) {
+      id
+      name
+    }
+  }
+`;
+
+export function NewUserForm() {
+  const [name, setName] = useState('');
+  const [createUser, { data, loading, error }] = useMutation(CREATE_USER);
+  
+  async function handleCreateUser(event: FormEvent) {
+    event.preventDefault();
+
+    if (!name) {
+      return;
+    };
+
+    await createUser({
+      variables: {
+        name
+      },
+    });
+
+    console.log(data);
+  };
+  
+  return (
+    <form onSubmit={handleCreateUser}>
+      <input type="text" value={name} onChange={e => setName(e.target.value)} />
+      <button type="submit">Enviar</button>
+    </form>
+  );
+};
